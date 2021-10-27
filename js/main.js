3f58578e74d3bb0d2e5267c11e3a6fad18a3692e
Mark Fishman <gibor1122@gmail.com>
2021-10-27 20:09:23 +0300

    third-commit

--- js/main.js
+++ js/main.js
@@ -6,10 +6,10 @@ var gCtx;
 function onInit() {
     gElCanvas = document.getElementById('my-canvas');
     gCtx = gElCanvas.getContext('2d');
-    renderMemes()
-
+    renderMemes();
+    renderControlBox();
     // resizeCanvas()
-
+    addListeners();
 }
 
 function renderMemes() {
@@ -25,28 +25,97 @@ function renderMemes() {
     document.querySelector('.memes-container').innerHTML = strHTML.join('');
 }
 
-function renderControlBox(count) {
-    var strHTML = `<input type="text" placeholder="${gMeme.lines[0].txt}" class="line-input">`
-    document.querySelector('.control-box').innerHTML = strHTML;
-
-
+function resizeCanvas() {
+    const elContainer = document.querySelector('.canvas')
+    gElCanvas.width = elContainer.offsetWidth
+    gElCanvas.height = elContainer.offsetHeight
 }
 
-function onDrawText(text, x, y) {
+function addListeners() {
+    document.getElementById('txt').addEventListener('input', getImputTxt);
+    addMouseListeners()
+    addTouchListeners()
+    // window.addEventListener('resize', () => {
+    //     resizeCanvas()
+    //     renderCanvas()
+    // })
+}
+function addMouseListeners() {
+    // gElCanvas.addEventListener('mousemove')
+    gElCanvas.addEventListener('mousedown', onDown)
+    // gElCanvas.addEventListener('mouseup')
+}
+
+function addTouchListeners() {
+    // gElCanvas.addEventListener('touchmove')
+    // gElCanvas.addEventListener('touchstart')
+    // gElCanvas.addEventListener('touchend')
+}
+//Event Listener Functions:
+function onDown(ev) {
+    const pos = getEvPos(ev)
+    if (!isTextClicked(pos)) return
+    setTextDrag(true)
+    gStartPos = pos
+    document.body.style.cursor = 'grabbing'
+}
+
+function getEvPos(ev) {
+    var pos = {
+        x: ev.offsetX,
+        y: ev.offsetY
+    }
+    if (gTouchEvs.includes(ev.type)) {
+        ev.preventDefault()
+        ev = ev.changedTouches[0]
+        pos = {
+            x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
+            y: ev.pageY - ev.target.offsetTop - ev.target.clientTop
+        }
+    }
+    return pos
+}
+
+function renderControlBox() {
+    var strHTML = `   <input type="text" id="txt" placeholder="${gMeme.lines[0].txt}" class="line-input">
+    <button class="switch-btn">switch lines</button>
+    <button class="add-btn">ADD LINE</button>
+    <button class="delete-btn">delete Line</button>`
+    document.querySelector('.control-box').innerHTML = strHTML;
+}
+
+function getImputTxt() {
+
+    renderCanvas();
+    var txt = document.querySelector('#txt').value;
+    gTextLength = txt.length
+    gMeme.lines[gSelectedLine].txt = txt;
+    var currMemeTxt = gMeme.lines[gSelectedLine].txt
+    onDrawText(currMemeTxt, 100, 100, 40);
+    console.log(gTextLength);
+}
+
+function renderCanvas() {
+    drawImg(gCurrMemeId);
+}
+
+function onDrawText(text, x, y, size) {
     gCtx.lineWidth = 2;
     gCtx.strokeStyle = 'black';
     gCtx.fillStyle = 'white';
-    gCtx.font = '40px Arial';
+    gCtx.font = `${size}px Impcat`
     gCtx.fillText(text, x, y);
     gCtx.strokeText(text, x, y);
 }
 
 function editPicture(count) {
+    gCurrMemeId = count;
     gMeme.selectedImgId = count;
     hideGallery();
     drawImg(count);
-    renderControlBox(count);
     changeFont()
+    onDrawText(gMeme.lines[0].txt, 100, 100, gMeme.lines[0].size)
+
 }
 
 function changeFont() {
@@ -58,6 +127,9 @@ function changeFont() {
 function drawImg(count) {
     var img = new Image();
     img.src = `imgs-square/${count}.jpg`
+    // img.onload = () => {
+    //     gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
+    // };
     gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
 }
 
--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,5 +1,7 @@
 'use strict'
 var gMemeId = 1;
+var gCurrMemeId;
+var gSelectedLine = 0;
 var gKeywords = { 'happy': 12, 'funny puk': 1 };
 var gImgs = [
     { id: gMemeId++, url: 'imgs-square/1.jpg', keywords: ['politics'] },
@@ -21,8 +23,28 @@ var gImgs = [
     { id: gMemeId++, url: 'imgs-square/17.jpg', keywords: ['politics'] },
     { id: gMemeId++, url: 'imgs-square/18.jpg', keywords: ['politics'] },
 ];
-var gMeme = { selectedImgId: 5, selectedLineIdx: 0, lines: [{ txt: 'I never eat Falafel', size: 20, align: 'left', color: 'red' }] };
+var gMeme = { selectedImgId: 5, selectedLineIdx: 0, lines: [{ txt: 'I never eat Falafel', size: 40, align: 'left', color: 'red' }, { txt: '', size: 40, align: 'left', color: 'red' }] };
+
+var gTextLength;
+const gTouchEvs = ['touchstart', 'touchmove', 'touchend'];
+
 
 function getImgs() {
     return gImgs;
 }
+
+function isTextClicked(clickedPos) {
+    console.log(clickedPos);
+    const fieldX = 100 + gTextLength * 19;
+    const filedY = 60;
+    if (clickedPos.x < fieldX && clickedPos.x >= 100 && clickedPos.y >= filedY && clickedPos.y <= 100) {
+        console.log('ON TEXT');
+    }
+
+
+}
+function setTextDrag(isDrag) {
+    //לעשות משתנה של שורה נוכחית שנבחרה ולהוסיף לגי מימ קיי של איז דראג
+    // gCircle.isDrag = isDrag
+}
+
