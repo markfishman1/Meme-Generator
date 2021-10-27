671398aa587b346490e551ed7fd610697a47cd78
Mark Fishman <gibor1122@gmail.com>
2021-10-27 12:18:55 +0300

    first commit

---
+++ js/controller.js
@@ -0,0 +1,5 @@
+'use strict'
+
+var gKeywords = { 'happy': 12, 'funny puk': 1 };
+var gImgs = [{ id: 1, url: 'imgs-square/1.jpg', keywords: ['politics'] }];
+var gMeme = { selectedImgId: 5, selectedLineIdx: 0, lines: [{ txt: 'I never eat Falafel', size: 20, align: 'left', color: 'red' }] };
